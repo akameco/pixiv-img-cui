af72d18b538d60b58a550aeb7004dd52f9698aa5
akameco <akameco.t@gmail.com>
2016-08-20 23:05:33 +0900

    Support stdin

--- cli.js
+++ cli.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 'use strict';
 const meow = require('meow');
+const getStdin = require('get-stdin');
 const pixivImg = require('pixiv-img');
 
 const cli = meow(`
@@ -11,12 +12,20 @@ const cli = meow(`
 	  $ pixiv-img https://i1.pixiv.net/img-original/img/2016/07/12/00/01/28/57862620_p0.jpg
 `);
 
-if (!cli.input[0]) {
-	console.log('require input');
+const input = cli.input[0];
+
+if (!input && process.stdin.isTTY) {
+	console.error('Input required');
 	process.exit(1);
 }
 
-pixivImg(cli.input[0]).catch(err => {
-	console.error(err);
-	process.exit(1);
-});
+const run = input =>
+	pixivImg(input).then(output => {
+		console.log(output);
+	});
+
+if (input) {
+	run(input);
+} else {
+	getStdin().then(str => run(str.trim()));
+}
